5bafd2e47dd21d0a381000b91b111d4e0c4b6448
Laura Tender <lau.tender@gmail.com>
2021-06-29 15:01:20 +0300

    Part 2

--- index.ts
+++ index.ts
@@ -1,22 +1,44 @@
-/* Part 1 */
+/* Part 2 */
 
 function isChanged(n: number): boolean{
-    if (n % 3 == 0)
-        return true;
-    if (n % 5 == 0)
-        return true;
+    for (let div of problemDivisors){
+        if (n % div == 0)
+            return true;
+    }
     return false;
 }
 
-function getMessage(n: number): string{
-    if (n % 15 == 0)
-        return "FizzBuzz";
-    if (n % 3 == 0)
-        return "Fizz";
-    if (n % 5 == 0)
-        return "Buzz";
-    return "Number does not have a specific message";
+function changeMessage(n:number, d: number, previousString :string): string{
+    if (n % d == 0){
+        let addedString: string = specificMessages.get(d) || "";
+        return previousString.concat(addedString);
+    }
+    return previousString;
 }
+
+function getMessage(n: number): string{
+    let lastString :string = "";
+    let problemDivisorsCopy :number[] = problemDivisors.slice();
+
+    if (n % 17 == 0){
+        problemDivisorsCopy.reverse();
+    }
+
+    for (let div of problemDivisorsCopy){
+        lastString = changeMessage(n, div, lastString);
+    }
+    return lastString;
+}
+
+const problemDivisors = [3, 5, 7, 11, 13];
+
+const specificMessages = new Map([
+    [3, "Fizz"],
+    [5, "Buzz"],
+    [7, "Bang"],
+    [11, "Bong"],
+    [13,"Fezz"]
+  ]);
 
 const maxNumber: number = 100;
 

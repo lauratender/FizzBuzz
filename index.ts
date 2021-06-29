9b3652f20210e85ea5f99b56255855fc092c8e77
Laura Tender <lau.tender@gmail.com>
2021-06-29 15:37:54 +0300

    Removed isChanged function

--- index.ts
+++ index.ts
@@ -1,28 +1,17 @@
 /* Part 1 */
 
-function isChanged(n: number): boolean{
-    if (n % 3 == 0)
-        return true;
-    if (n % 5 == 0)
-        return true;
-    return false;
-}
-
-function getMessage(n: number): string{
-    if (n % 15 == 0)
+function getMessage(currNr: number): string{
+    if (currNr % 15 == 0)
         return "FizzBuzz";
-    if (n % 3 == 0)
+    if (currNr % 3 == 0)
         return "Fizz";
-    if (n % 5 == 0)
+    if (currNr % 5 == 0)
         return "Buzz";
-    return "Number does not have a specific message";
+    return currNr.toString();
 }
 
 const maxNumber: number = 100;
 
 for (let i = 1; i <= maxNumber; i++){
-    if (isChanged(i))
-        console.log(getMessage(i));
-    else
-        console.log(i);
+    console.log(getMessage(i));
 }
